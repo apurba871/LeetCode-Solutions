dd7daefbd6af0e5340cc26177968dfc14fcac2f1
Apurba Debnath <apurba871@gmail.com>
2026-05-20 08:08:06 +0530

    Create

---
+++ problem-1779244685033/version/java/problem-1779244685033_20260520_080805.java
@@ -0,0 +1,20 @@
+// Last updated: 5/20/2026, 8:08:05 AM
+class Solution {
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        int[] ans = new int[A.length];
+        int[] freq = new int[A.length + 1];
+        int common = 0;
+        for(int i = 0; i < A.length; i++){
+            freq[A[i]]++;
+            if(freq[A[i]] == 2){
+                common++;
+            }
+            freq[B[i]]++;
+            if(freq[B[i]] == 2){
+                common++;
+            }
+            ans[i] = common;
+        }
+        return ans;
+    }
+}
