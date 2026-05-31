2d36f9756fc829f171e0380fcda5f6761781cb83
Apurba Debnath <apurba871@gmail.com>
2026-05-31 06:49:23 +0530

    Create 2126-DestroyingAsteroids

---
+++ 2126-DestroyingAsteroids/version/java/2126-DestroyingAsteroids_20260531_064923.java
@@ -0,0 +1,22 @@
+// Last updated: 5/31/2026, 6:49:23 AM
+1class Solution {
+2    static {
+3        for (int i = 0; i < 1000; ++i) {
+4            asteroidsDestroyed(1, new int[]{});
+5        }
+6    }
+7    public static boolean asteroidsDestroyed(int mass, int[] asteroids) {
+8        Arrays.sort(asteroids);
+9        boolean flag = true;
+10        long currMass = mass;
+11        for (int asteroid : asteroids) {
+12            if (currMass >= asteroid) {
+13                currMass += asteroid;
+14            } else {
+15                flag = false;
+16                break;
+17            }
+18        }
+19        return flag;
+20    }
+21}
