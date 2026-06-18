f8a2723943a9774357081c07ff15ae37bce5f233
Apurba Debnath <apurba871@gmail.com>
2026-06-18 08:09:45 +0530

    Create 1344-AngleBetweenHandsofaClock

---
+++ 1344-AngleBetweenHandsofaClock/version/java/1344-AngleBetweenHandsofaClock_20260618_080945.java
@@ -0,0 +1,8 @@
+// Last updated: 6/18/2026, 8:09:45 AM
+1class Solution {
+2    public double angleClock(int hour, int minutes) {
+3        double x = hour + minutes / 60.0;
+4        double diff = (11.0 * x) % 12.0;
+5        return Math.min(diff, 12.0 - diff) * 30.0;
+6    }
+7}
